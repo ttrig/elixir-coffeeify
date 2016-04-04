b9b031a6bfa119ac8b621df55a0567c03f684470
Markus <ttrig@users.noreply.github.com>
2016-04-04 15:02:32 +0200

    Update coffeeify.js

--- coffeeify.js
+++ coffeeify.js
@@ -7,36 +7,38 @@ var coffeeify  = require('coffeeify');
 var config     = Elixir.config;
 
 Elixir.extend('coffeeify', function(src, output, options) {
+	var paths = prepGulpPaths(src, output);
 
 	new Elixir.Task('coffeeify', function() {
-		var paths = prepGulpPaths(src, output);
-		var files = paths.src.path;
+		this.log(paths.src, paths.output);
 
+		var files = paths.src.path;
 		if (files.constructor !== Array) {
 			files = [files];
 		}
 
 		function make(file) {
-				var outputFile = path.basename(file).replace('.coffee','.js');
-				return browserify(file, {
-					extensions : ['.coffee']
-				})
-				.transform(coffeeify)
-				.bundle()
-				.on('error', function(e) {
-					new Elixir.Notification().error(e, 'CoffeeScript Compilation Failed!');
-					this.emit('end');
-				})
-				.pipe(source(outputFile))
-				.pipe(gulp.dest(paths.output.baseDir))
-				.pipe(new Elixir.Notification('CoffeeScript Compiled!'));
+			var outputFile = path.basename(file).replace('.coffee','.js');
+			return browserify(file, {
+				extensions: ['.coffee'],
+				debug: config.production ? false : true
+			})
+			.transform(coffeeify)
+			.bundle()
+			.on('error', function(e) {
+				new Elixir.Notification().error(e, 'CoffeeScript Compilation Failed!');
+				this.emit('end');
+			})
+			.pipe(source(outputFile))
+			.pipe(gulp.dest(paths.output.baseDir))
+			.pipe(new Elixir.Notification('CoffeeScript Compiled [coffeeify]!'));
 		}
 
 		return files.map(make);
 	})
-	.watch(config.get('assets.js.coffee.folder') + '/**/*.coffee');
+	.watch(config.get('assets.js.coffee.folder') + '/**/*.coffee')
+	.ignore(paths.output.path);
 });
-
 
 /**
  * Prep the Gulp src and output paths.
